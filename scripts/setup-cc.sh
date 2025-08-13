000eb0dc667238b31c6e884ef120b055c2456d83
Alexander Ott <66271487+AlexanderHott@users.noreply.github.com>
2025-08-13 17:10:51 -0400

    add setup script

---
+++ scripts/setup-cc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+echo "Installing system dependencies"
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install git curl build-essential cmake sysstat libgflags-dev -y
+
+echo "Installing rust"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+. "$HOME/.cargo/env"
+rustup default nightly
+
+echo "Installing uv (for python)"
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+echo "Setting up python"
+cd vis
+uv sync
+cd ..
+
+echo "Setting up cmake"
+./reload-cmake.sh
