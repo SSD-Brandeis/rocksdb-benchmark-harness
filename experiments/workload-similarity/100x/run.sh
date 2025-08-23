b8a5a052a2725245985ff46e0c673e9feb0644ee
Alexander Ott <66271487+AlexanderHott@users.noreply.github.com>
2025-08-23 16:38:52 -0400

    add 10x and 100x workloads and correct update value len

---
+++ experiments/workload-similarity/100x/run.sh
@@ -0,0 +1,103 @@
+
+#!/bin/bash
+
+set -e
+
+RUNS=5
+EXPERIMENT_PATH=$(realpath "${EXPERIMENT_PATH}/100x/")
+
+echo "Building tectonic"
+cd vendor/tectonic
+CARGO_TARGET_DIR="target" cargo build --release
+cd ../..
+
+echo "Building rocksdb-benchmark-harness"
+cmake --build cmake-build-release --target rocksdb-benchmark-harness -- -j"$(nproc)"
+cmake --build cmake-build-release-with-stats --target rocksdb-benchmark-harness -- -j"$(nproc)"
+
+function generate_tectonic_workload() {
+  echo "generating tectonic workload"
+  ./vendor/tectonic/target/release/tectonic-cli generate \
+    -w "${EXPERIMENT_PATH}/workload-a.spec.json" \
+    -o "${EXPERIMENT_PATH}/tec-workload-a.txt"
+}
+
+
+function generate_ycsb_workload() {
+  echo "generating ycsb workload"
+  cd vendor/YCSB
+  /usr/lib/jvm/java-17-openjdk-amd64/bin/java \
+    -cp /home/ott/Documents/code/ssd/rocksdb-benchmark-harness/vendor/YCSB/file/conf:/home/ott/Documents/code/ssd/rocksdb-benchmark-harness/vendor/YCSB/file/target/file-binding-0.18.0-SNAPSHOT.jar:/home/ott/.m2/repository/org/apache/htrace/htrace-core4/4.1.0-incubating/htrace-core4-4.1.0-incubating.jar:/home/ott/.m2/repository/org/hdrhistogram/HdrHistogram/2.1.12/HdrHistogram-2.1.12.jar:/home/ott/.m2/repository/org/codehaus/jackson/jackson-mapper-asl/1.9.4/jackson-mapper-asl-1.9.4.jar:/home/ott/.m2/repository/org/codehaus/jackson/jackson-core-asl/1.9.4/jackson-core-asl-1.9.4.jar:/home/ott/Documents/code/ssd/rocksdb-benchmark-harness/vendor/YCSB/core/target/core-0.18.0-SNAPSHOT.jar site.ycsb.Client \
+    -db site.ycsb.db.FileClient \
+    -P workloads/workloada \
+    -p "file.output=${EXPERIMENT_PATH}/ycsb-workload.1.part" \
+    -p recordcount=100000000 \
+    -p operationcount=100000000 \
+    -load
+
+  /usr/lib/jvm/java-17-openjdk-amd64/bin/java \
+    -cp /home/ott/Documents/code/ssd/rocksdb-benchmark-harness/vendor/YCSB/file/conf:/home/ott/Documents/code/ssd/rocksdb-benchmark-harness/vendor/YCSB/file/target/file-binding-0.18.0-SNAPSHOT.jar:/home/ott/.m2/repository/org/apache/htrace/htrace-core4/4.1.0-incubating/htrace-core4-4.1.0-incubating.jar:/home/ott/.m2/repository/org/hdrhistogram/HdrHistogram/2.1.12/HdrHistogram-2.1.12.jar:/home/ott/.m2/repository/org/codehaus/jackson/jackson-mapper-asl/1.9.4/jackson-mapper-asl-1.9.4.jar:/home/ott/.m2/repository/org/codehaus/jackson/jackson-core-asl/1.9.4/jackson-core-asl-1.9.4.jar:/home/ott/Documents/code/ssd/rocksdb-benchmark-harness/vendor/YCSB/core/target/core-0.18.0-SNAPSHOT.jar site.ycsb.Client \
+    -db site.ycsb.db.FileClient \
+    -P workloads/workloada \
+    -p "file.output=${EXPERIMENT_PATH}/ycsb-workload.2.part" \
+    -p recordcount=100000000 \
+    -p operationcount=100000000 \
+    -t
+
+  cat "${EXPERIMENT_PATH}/ycsb-workload.1.part" "${EXPERIMENT_PATH}/ycsb-workload.2.part" >"${EXPERIMENT_PATH}/ycsb-workload-a.txt"
+  rm "${EXPERIMENT_PATH}/ycsb-workload.1.part" "${EXPERIMENT_PATH}/ycsb-workload.2.part"
+  cd ../..
+}
+
+for i in $(seq 1 "$RUNS"); do
+  echo "tectonic iostat run $i"
+  generate_tectonic_workload
+  generate_ycsb_workload
+
+  iostat -d -c -y 1 nvme0n1 -o JSON >"${EXPERIMENT_PATH}/iostat.tectonic.$i.json" &
+  IOSTAT_PID=$!
+  sudo sysctl -w vm.drop_caches=3
+
+  ./cmake-build-release/rocksdb-benchmark-harness \
+   "${EXPERIMENT_PATH}/rocksdb-options.ini" \
+   "${EXPERIMENT_PATH}/tec-workload-a.txt"
+
+  kill -INT $IOSTAT_PID
+
+  echo "YCSB iostat run $i"
+  iostat -d -c -y 1 nvme0n1 -o JSON >"${EXPERIMENT_PATH}/iostat.ycsb.$i.json" &
+  IOSTAT_PID=$!
+  sudo sysctl -w vm.drop_caches=3
+
+  ./cmake-build-release/rocksdb-benchmark-harness \
+   "${EXPERIMENT_PATH}/rocksdb-options.ini" \
+   "${EXPERIMENT_PATH}/ycsb-workload-a.txt"
+
+  kill -INT $IOSTAT_PID
+done
+
+for i in $(seq 1 "$RUNS"); do
+  echo "generating workloads for run $i"
+  generate_tectonic_workload
+  generate_ycsb_workload
+
+  echo "tectonic stats run $i"
+  sudo sysctl -w vm.drop_caches=3
+
+  ./cmake-build-release-with-stats/rocksdb-benchmark-harness \
+   "${EXPERIMENT_PATH}/rocksdb-options.ini" \
+   "${EXPERIMENT_PATH}/tec-workload-a.txt" \
+   "${EXPERIMENT_PATH}/stats.tectonic.$i.json" \
+   "${EXPERIMENT_PATH}/op-latency.tectonic.$i.json"
+
+
+  echo "YCSB stats run $i"
+  sudo sysctl -w vm.drop_caches=3
+
+  ./cmake-build-release-with-stats/rocksdb-benchmark-harness \
+   "${EXPERIMENT_PATH}/rocksdb-options.ini" \
+   "${EXPERIMENT_PATH}/ycsb-workload-a.txt" \
+   "${EXPERIMENT_PATH}/stats.ycsb.$i.json" \
+   "${EXPERIMENT_PATH}/op-latency.ycsb.$i.json"
+
+done
